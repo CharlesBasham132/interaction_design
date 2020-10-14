561e820c47f9f582660a9e9cb23a78a727aaa9a2
Ben Kilgariff <charliefred132@gmail.com>
2020-10-14 10:56:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import week1 from "./components/week1";
+
+const App = () => (
+  <>
+      <week1 />
+  </>
+
+);
+
+export default App;
---
+++ src/components/week1/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TopLeftTriangle from "./assets/top_left_triangle.png";
+
+const week1 = () => (
+    <>
+        <img src={TopLeftTriangle} />
+    </>
+);
+
+export default week1;
