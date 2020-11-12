90fcc9f4ba9836dfdafd4416f2570bd96b60a853
Ben Kilgariff <charliefred132@gmail.com>
2020-11-12 16:13:06 +0000

    Updates

---
+++ precache-manifest.5699a51b162b2e097944e77a84d75c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480e75a4b26c3d8dabe7aac70538484f",
+    "url": "/interaction_design/index.html"
+  },
+  {
+    "revision": "09f42672bcbd8a0912c8",
+    "url": "/interaction_design/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "800aaa7b930935bc9d7d",
+    "url": "/interaction_design/static/js/2.7f52ad1b.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/interaction_design/static/js/2.7f52ad1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09f42672bcbd8a0912c8",
+    "url": "/interaction_design/static/js/main.b306dd4d.chunk.js"
+  },
+  {
+    "revision": "e25be7ba49f70be42cca",
+    "url": "/interaction_design/static/js/runtime-main.43e566e4.js"
+  }
+]);
